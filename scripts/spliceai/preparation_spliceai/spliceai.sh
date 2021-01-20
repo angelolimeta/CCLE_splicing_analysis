81c09198e82ef71167a39da5269297bfc273e76e
leticiacastillon <60732799+leticiacastillon@users.noreply.github.com>
2021-01-20 22:41:32 +0100

    Adding scripts for differential expression and linear model

---
+++ scripts/spliceai/preparation_spliceai/spliceai.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH -A C3SE2020-1-8
+#SBATCH -p hebbe
+#SBATCH -n 6 -t 0-1:30:00
+
+module load Anaconda3
+echo "Activating conda environment"
+source activate /cephyr/NOBACKUP/groups/snic2020-8-84/conda/envs/spliceai
+
+echo "running spliceai"
+
+input="/cephyr/NOBACKUP/groups/snic2020-8-84/spliceai/dummy_out.vcf.gz"
+output="/cephyr/NOBACKUP/groups/snic2020-8-84/spliceai/output/splice_dummy_out.vcf"
+reference="/cephyr/NOBACKUP/groups/snic2020-8-84/spliceai/ref/hg38.fa"
+ann="/cephyr/NOBACKUP/groups/snic2020-8-84/spliceai/annotations/grch38.txt"
+
+spliceai -I $input -O $output -R $reference -A $ann
